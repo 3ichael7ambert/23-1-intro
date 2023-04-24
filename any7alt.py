61cdf0fae44af065bbd8b91a2590bc2f90df2ba3
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-04-23 20:24:15 -0600

    Create any7alt.py

---
+++ any7alt.py
@@ -0,0 +1,18 @@
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+    if 7 in nums:
+        return True
+    else:
+        return False
+
+print("should be True:", any7([1, 2, 7, 4, 5]))
+print("should be False:", any7([1, 2, 4, 5]))
+def any7(nums):
+    """Are any of these numbers a 7? (True/False)"""
+    if 7 in nums:
+        return True
+    else:
+        return False
+
+print("should be True:", any7([1, 2, 7, 4, 5]))
+print("should be False:", any7([1, 2, 4, 5]))
